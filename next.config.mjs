aca755c7e5bbb26ddd46d5da0ea951c423ab88a8
krisshsaahi49 <krisshsaahi@outlook.com>
2024-12-29 15:19:48 -0500

    updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true, // Enable the 'app/' directory routing
-    },
   };
   
   export default nextConfig;
